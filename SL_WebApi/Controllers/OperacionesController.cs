c15b37391e65cde4064ff173ff44d11b47969898
JoseRBP <josera.jack@gmail.com>
2024-08-07 12:15:54 -0600

    WebApi

---
+++ SL_WebApi/Controllers/OperacionesController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SL_WebApi.Controllers
+{
+    public class OperacionesController : ApiController
+    {
+        //Status Code + Informacion
+        //Investigar los status code de http
+        //200 aceptacion // Correcto
+
+        //Route  ULR para acceder a este metodo
+        [Route("api/Operaciones/Saludar")]
+        [HttpGet]
+        public IHttpActionResult Saludar(string nombre)
+        {
+            return Ok("Hola " + nombre);
+        }
+
+        [Route("api/Operaciones/Sumar")]
+        [HttpPost]
+        public IHttpActionResult Sumar(int numero1, int numero2)
+        {
+            return Ok(numero1+numero2);
+        }
+    }
+}
